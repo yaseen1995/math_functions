c74d8f47f212902a220af005da547e32f1d24e7c
Yaseen Patel <P14159245@GWHXX581LIN005W.lec-admin.dmu.ac.uk>
2016-02-12 13:57:17 +0000

    I added a function.

---
+++ maths_function.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int addition(int x, int y) {
+	return x+y;
+}
+
+int subtraction(int x, int y) {
+	return x-y;
+}
+
+int multiplication(int x, int y) {
+	return x*y;
+}
+
+float division(int x, int y) {
+	return x/y;
+}
+
+int remainder(int x, int y) {
+	return x%y;
+}
+
+int min (int x, int y)
+{
+	if (x>y)
+	return x;
+}
+
+int main() {
+	int x=2, y=3;
+	printf("%i\n\n", addition(x,y));
+	printf("%i\n\n", subtraction(x,y));
+	printf("%i\n\n", multiplication(x,y));
+	printf("%f\n", division(x,y));
+	printf("%f\n", remainder(x,y));
+	return 0;
+}
